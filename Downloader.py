ef681de5eb958db5a744d0b727fe7d425c71cafe
Shmilowitz <sandwichmanden@hotmail.com>
2017-03-28 22:13:14 +0200

    Add files via upload

---
+++ Downloader.py
@@ -0,0 +1,5 @@
+import numpy as np, matplotlib.pyplot as plt, webget, pandas as pd,collections
+
+
+# WEBGET ONLY USE ONCE TO DONWLOAD VGSALES.CSV
+webget.download('https://raw.githubusercontent.com/edipetres/Depressed_Year/master/Dataset_Assignment/AviationDataset.csv')
