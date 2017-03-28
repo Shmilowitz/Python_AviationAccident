23e2e59a5508254ea6195ef02bc770f3fa6aaf8b
Shmilowitz <sandwichmanden@hotmail.com>
2017-03-28 22:12:04 +0200

    Delete Q3.py

--- Q3.py
+++
@@ -1,9 +0,0 @@
-import numpy as np, matplotlib.pyplot as plt, pandas as pd,collections, sys, csv, urllib.request
-from collections import OrderedDict
-import operator
-database = pd.read_csv('Aviation.csv',quotechar='"',skipinitialspace=True, delimiter=',', encoding='latin1').fillna(0)
-data = database.as_matrix()
-
-d = collections.defaultdict(list)
-maskUS =  (data[:,5] == 'United States')
-print(data[:,3])
